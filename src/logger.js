aac4df9c85f9667824195f45013c38cdb25d3c05
bluepill5 <alexandro.mayoral@gmail.com>
2022-06-11 23:49:22 -0500

    Adding logger

---
+++ src/logger.js
@@ -0,0 +1,10 @@
+import log4js from 'log4js';
+
+log4js.configure({
+
+});
+
+let logger = log4js.getLogger();
+
+export default logger;
+
--- src/routes/info.routes.js
+++ src/routes/info.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import * as InfoController from '../controllers/info.controllers.js';
+import compression from 'compression';
 
 const router = express.Router();
 
-router.get('/info', InfoController.getInfo);
+router.get('/info', compression(), InfoController.getInfo);
 
 export default router;
 
